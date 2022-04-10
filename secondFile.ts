a340a716dc253eaaa16b401ad712e38693e4dc58
sudhanIOS <whari17@gmail.com>
2022-04-10 16:30:39 +0530

    resolve conflict

--- secondFile.ts
+++ secondFile.ts
@@ -2,12 +2,7 @@ type A = {
     x: Number;
     z: Number;
     y: Number;
-<<<<<<< Updated upstream
-    
-    
     B:String;
-=======
-
     A:string;
->>>>>>> Stashed changes
+    //new added
 }
